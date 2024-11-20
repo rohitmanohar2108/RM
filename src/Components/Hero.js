abd84d26dd36d5321b2c99dca7b892f395667d3d
Rohit Manohar <manoharrohit372@gmail.com>
2024-11-20 19:50:40 +0530

    d ep

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -87,7 +87,7 @@ export default function Hero() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 1.2, duration: 1.5 }}
-        className="absolute bottom-10 left-1/2 transform -translate-x-1/2 "
+        className="absolute bottom-10 left-1/2 transform -translate-x-1/2 mb-9"
       >
         <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
           <motion.div
